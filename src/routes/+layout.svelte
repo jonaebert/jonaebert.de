6841916716ec5eb76d8f54bdda8a9f49c5f29482
Jona Ebert <kontakt@jonaebert.de>
2025-03-30 20:27:58 +0200

    Added margin-right to burger menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -135,7 +135,7 @@
 				<Image src={logo} alt="Logo von {name}" classNames="h-14 md:h-16 duration-500 ease-in-out transition-transform transform scale-105 hover:scale-110" />
 				<!-- <span class="self-center text-2xl font-semibold whitespace-nowrap">Jona Ebert</span> -->
 			</a>
-			<button class="group md:hidden inline-flex w-12 h-12 text-center items-center justify-center rounded transition" aria-pressed="false" on:click={toggleMenu} type="button">
+			<button class="group md:hidden inline-flex w-12 h-12 text-center items-center justify-center rounded transition mr-3" aria-pressed="false" on:click={toggleMenu} type="button">
 				<span class="sr-only">Menu</span>
 				<svg class="w-6 h-6 fill-current pointer-events-none" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
 					<rect class="origin-center -translate-y-[5px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-[[aria-pressed=true]]:translate-y-0 group-[[aria-pressed=true]]:rotate-[135deg]" y="7" width="16" height="1.5" rx="1"></rect>
