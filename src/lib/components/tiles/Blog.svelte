4c05dc251ea31fee8101494ea3cfc87cda506c8e
Jona Ebert <kontakt@jonaebert.de>
2025-04-13 20:50:46 +0200

    Added shrink-0

--- src/lib/components/tiles/Blog.svelte
+++ src/lib/components/tiles/Blog.svelte
@@ -24,7 +24,7 @@
                     <!-- {#if item.location} -->
                     <div class="mb-1 flex items-center text-xs text-je-secondary-600 md:text-base hyphens-auto text-pretty font-montserrat">
                         <div class="flex items-center">
-                            <svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 119 119" class="mr-1.5 inline h-4 w-4 fill-je-secondary-600">
+                            <svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 119 119" class="mr-1.5 shrink-0 inline h-4 w-4 fill-je-secondary-600">
                                 <path fill="inherit" d="M97.1,119H21.9C9.8,119,0,109.2,0,97.1V21.9C0,9.8,9.8,0,21.9,0h75.3C109.2,0,119,9.8,119,21.9v75.3 C119,109.2,109.2,119,97.1,119L97.1,119z M21.9,11c-6,0-10.9,4.9-10.9,10.9v75.3c0,6,4.9,10.9,10.9,10.9h75.3 c6,0,10.9-4.9,10.9-10.9V21.9c0-6-4.9-10.9-10.9-10.9H21.9z"></path>
                                 <rect x="26.3" y="26.5" fill="inherit" width="65.7" height="11"></rect>
                                 <rect x="26.3" y="54" fill="inherit" width="65.7" height="11"></rect>
