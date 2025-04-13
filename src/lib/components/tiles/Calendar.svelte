af3c8f53b90ede093f8b00ba9a5dafed45fae40e
Jona Ebert <kontakt@jonaebert.de>
2025-04-13 20:52:41 +0200

    Added location icon for location at calendar item

--- src/lib/components/tiles/Calendar.svelte
+++ src/lib/components/tiles/Calendar.svelte
@@ -41,6 +41,9 @@
                     {#if event.location}
                         <div class="mb-1 flex items-center text-xs text-je-secondary-600 md:text-base hyphens-auto text-pretty font-montserrat">
                             <div class="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 -960 960 960" class="mr-1.5 shrink-0 inline h-4 w-4 fill-je-secondary-600">
+                                    <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/>
+                                </svg>
                                 {event.location}
                             </div>
                         </div>
