2d7cb79f1c732204b0f0ff1fc2eff313b625d9ce
Jona Ebert <kontakt@jonaebert.de>
2025-05-03 22:24:29 +0200

    Added je_api_base_url from env

--- src/lib/store.js
+++ src/lib/store.js
@@ -22,6 +22,7 @@ export const uri = page;
 
 // API
 export const apiDomain = 'api.jonaebert.de';
+export const je_api_base_url = import.meta.env.VITE_JE_API_BASE_URL;
 export const bb_base_url = import.meta.env.VITE_BB_BASE_URL;
 export const bb_api_token = import.meta.env.VITE_BB_API_TOKEN;
 
