32516e66fe8a4f884ca70470381c6aea7453cd3a
Jona Ebert <kontakt@jonaebert.de>
2025-06-02 21:13:19 +0200

    Revert "First implementation for image optimizer"
    This reverts commit 135607c60e606f014d175c8403803ae3dbf4ea9f.

--- src/lib/components/image.svelte
+++ src/lib/components/image.svelte
@@ -1,40 +1,8 @@
-<!-- <script lang="ts">
-	import { contextMenuAction, uri } from '$lib/store';
-	import imageLoader from '$lib/util/imageLoader';
-
-	export let src: string;
-	export let alt: string = '';
-	export let classNames: string = '';
-</script>
-
-<img {src} {alt} class={classNames} use:contextMenuAction /> -->
-
 <script lang="ts">
-	import { contextMenuAction, uri } from '$lib/store';
-	import imageLoader from '$lib/util/imageLoader';
-
-	export let src: string;
-	export let alt: string = '';
-	export let classNames: string = '';
-	export let classes: string = '';
-
-	if (classes) {
-		classes = classes;
-	} else if (classNames) {
-		classes = classNames;
-	}
-
-	// Bildoptimierung konfigurieren
-	let optimizedSrc: string;
-
-	// `optimizedSrc` vorbereiten, sobald sich `src` ändert
-	$: optimizedSrc = imageLoader({
-		uri: $uri,
-		src,
-		width: 1500,
-		quality: 75 // Optional
-	});
-	console.log(optimizedSrc);
+    import { contextMenuAction } from '$lib/store';
+    export let src: string;
+    export let alt: string = '';
+    export let classNames: string = '';
 </script>
-
-<img src={optimizedSrc} {alt} class={classes} use:contextMenuAction />
+  
+<img {src} {alt} class={classNames} use:contextMenuAction />
