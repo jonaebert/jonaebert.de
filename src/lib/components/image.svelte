f8ba2160718652ecabaad19e92e379b270cd87e4
Jona Ebert <kontakt@jonaebert.de>
2025-06-02 22:52:28 +0200

    Added copyright to image component

--- src/lib/components/image.svelte
+++ src/lib/components/image.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
-    import { contextMenuAction } from '$lib/store';
-    export let src: string;
-    export let alt: string = '';
-    export let classNames: string = '';
+	import { contextMenuAction, name } from '$lib/store';
+	export let src: string;
+	export let alt: string = '';
+	export let classNames: string = '';
+	export let copyright: { name: string; url: string }[] = [{ name: '', url: '' }];
 </script>
-  
-<img {src} {alt} class={classNames} use:contextMenuAction />
+
+<div class="relative inline-block">
+	<img {src} {alt} class={classNames} use:contextMenuAction />
+	{#if copyright.length > 0 && copyright[0].name !== ''}
+		<div class="absolute right-2 bottom-2 bg-grey-50 rounded p-1 text-xs text-black opacity-75">
+			{#if copyright[0].url !== '' && copyright[0].url !== undefined && copyright[0].url.startsWith('https://')}
+				<a href={copyright[0].url} target="_blank">
+					<p class="text-balance text-end">
+						&copy;
+						{copyright[0].name}
+					</p>
+				</a>
+			{:else}
+				<p class="text-balance text-end">
+					&copy;
+					{copyright[0].name}
+				</p>
+			{/if}
+		</div>
+	{/if}
+</div>
--- src/lib/components/prismic/image.svelte
+++ src/lib/components/prismic/image.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
-    import * as ph from '@prismicio/helpers';
-    import Image from '../image.svelte';
+	import * as ph from '@prismicio/helpers';
+	import Image from '../image.svelte';
 
-    export let slice;
+	export let slice;
 </script>
 
 <div>
-    <Image src={ph.asImageSrc(slice) ?? ''} alt={slice.alt ?? ''} classNames="h-auto w-80 rounded-lg" />
-    {#if slice.copyright}
-        {#if slice.linkTo}
-            <div>
-                <p>📸 <a href={slice.linkTo.url} target={slice.linkTo.target}>{slice.copyright}</a></p>
-            </div>
-        {:else}
-            <div>
-                <p>📸 {slice.copyright}</p>
-            </div>
-        {/if}
-    {/if}
+	{#if slice.copyright}
+		{#if slice.linkTo}
+			<Image
+				src={ph.asImageSrc(slice) ?? ''}
+				alt={slice.alt ?? ''}
+				classNames="h-auto w-80 rounded-lg"
+				copyright={[{ name: slice.copyright, url: slice.linkTo.url }]}
+			/>
+		{:else}
+			<Image
+				src={ph.asImageSrc(slice) ?? ''}
+				alt={slice.alt ?? ''}
+				classNames="h-auto w-80 rounded-lg"
+				copyright={[{ name: slice.copyright, url: '' }]}
+			/>
+		{/if}
+	{:else}
+		<Image
+			src={ph.asImageSrc(slice) ?? ''}
+			alt={slice.alt ?? ''}
+			classNames="h-auto w-80 rounded-lg"
+			copyright={[{ name: '', url: '' }]}
+		/>
+	{/if}
 </div>
