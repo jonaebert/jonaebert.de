e66b4406b105e3167317f9940560fa52bbcfb467
Jona Ebert <kontakt@jonaebert.de>
2025-07-13 16:13:33 +0200

    Füge die TextBlock-Komponente und den Renderer hinzu: Implementiere die Logik zur Anzeige von Blöcken und verwende die TextBlock-Komponente für 'shared.rich-text'.

---
+++ src/lib/components/stripe/Renderer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import InfoMessage from '$lib/components/blocks/InfoMessage.svelte';
+	import { components } from './components';
+
+	export let blocks: Array<any> = [];
+	const fallback = ({ __component }) => `Unbekannter Block: ${__component}`;
+
+	console.log('Renderer blocks:', blocks);
+</script>
+
+{#each blocks as block, i}
+	{#if components[block.__component]}
+		<svelte:component this={components[block.__component]} {...block} />
+	{:else}
+		<div class="flex text-center font-montserrat justify-start my-2">
+			<InfoMessage message={fallback(block)}></InfoMessage>
+		</div>
+	{/if}
+{/each}
---
+++ src/lib/components/stripe/components.ts
@@ -0,0 +1,5 @@
+import TextBlock from "./TextBlock.svelte";
+
+export const components: Record<string, any> = {
+    'shared.rich-text': TextBlock,
+};
