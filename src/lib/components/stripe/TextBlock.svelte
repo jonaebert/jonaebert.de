29455ed4140249031bd9f1436eea46d4666c138f
Jona Ebert <kontakt@jonaebert.de>
2025-07-13 19:31:22 +0200

    Füge die TextBlock-Komponente hinzu: Implementiere die Marked-Bibliothek zur Verarbeitung von Markdown-Inhalten.

--- src/lib/components/stripe/TextBlock.svelte
+++ src/lib/components/stripe/TextBlock.svelte
@@ -1,1 +1,26 @@
-TESTZ
+<script lang="ts">
+	import { marked } from 'marked';
+
+	export let body: string;
+
+	const renderer = {
+		heading(text, level) {
+			const classes = {
+				2: 'text-3xl md:text-4xl font-bold text-secondary-900 font-poppins italic',
+				3: 'text-2xl md:text-3xl font-semibold text-black font-poppins italic',
+				4: 'text-xl md:text-2xl font-semibold text-black font-poppins italic',
+				5: 'text-md md:text-xl font-semibold text-black font-poppins italic',
+				6: 'text-md md:text-xl font-medium text-black font-poppins italic'
+			};
+			text.depth = 1;
+			text.depth = text.depth === 1 ? 2 : text.depth;
+			const tag = `h${text.depth}`;
+			return `<${tag} class="${classes[text.depth]}">${text.text}</${tag}>`;
+		}
+	};
+
+	marked.use({ renderer });
+	$: html = marked.parse(body || '');
+</script>
+
+<div class="max-w-none mb-6">{@html html}</div>
