a3f1d9ce85b4e7005ad4e34f0b6a5ed37c0d7973
Jona Ebert <kontakt@jonaebert.de>
2025-07-13 16:35:22 +0200

    Verbessere die Fehlerbehandlung im Renderer: Entferne die Fallback-Funktion und verwende eine direkte Nachricht für unbekannte Blöcke.

--- src/lib/components/stripe/Renderer.svelte
+++ src/lib/components/stripe/Renderer.svelte
@@ -3,17 +3,14 @@
 	import { components } from './components';
 
 	export let blocks: Array<any> = [];
-	const fallback = ({ __component }) => `Unbekannter Block: ${__component}`;
-
-	console.log('Renderer blocks:', blocks);
 </script>
 
-{#each blocks as block, i}
+{#each blocks as block}
 	{#if components[block.__component]}
 		<svelte:component this={components[block.__component]} {...block} />
 	{:else}
 		<div class="flex text-center font-montserrat justify-start my-2">
-			<InfoMessage message={fallback(block)}></InfoMessage>
+			<InfoMessage message = 'Unbekannter Block: {block.__component}'></InfoMessage>
 		</div>
 	{/if}
 {/each}
