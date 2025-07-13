f76b4572ba6a5ec18ce09fd083ccad475fff310d
Jona Ebert <kontakt@jonaebert.de>
2025-07-13 19:30:50 +0200

    Füge das Zitat-Import hinzu: Bereite die Integration der Quote-Komponente vor.

--- src/lib/components/stripe/components.ts
+++ src/lib/components/stripe/components.ts
@@ -1,5 +1,7 @@
+import Quote from "./Quote.svelte";
 import TextBlock from "./TextBlock.svelte";
 
 export const components: Record<string, any> = {
     'shared.rich-text': TextBlock,
+    // 'shared.quote': Quote
 };
