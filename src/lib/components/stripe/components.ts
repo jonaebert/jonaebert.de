8e071ca5c1255bde5491068dc0074b55a2055a7b
Jona Ebert <kontakt@jonaebert.de>
2025-07-14 15:39:48 +0200

    Änderung Dateinamen für Image

--- src/lib/components/stripe/components.ts
+++ src/lib/components/stripe/components.ts
@@ -1,6 +1,6 @@
 import TextBlock from "./TextBlock.svelte";
 import Quote from "./Quote.svelte";
-import Image from "./tset.svelte";
+import Image from "./Image.svelte";
 
 export const components: Record<string, any> = {
     'shared.rich-text': TextBlock,
