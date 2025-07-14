44c52588bd1a03758f8a3094bdc82cb84be60339
Jona Ebert <kontakt@jonaebert.de>
2025-07-14 15:39:17 +0200

    Änderungen für Dateinamen Image

--- src/lib/components/stripe/components.ts
+++ src/lib/components/stripe/components.ts
@@ -1,7 +1,8 @@
 import TextBlock from "./TextBlock.svelte";
 import Quote from "./Quote.svelte";
+import Image from "./tset.svelte";
 
 export const components: Record<string, any> = {
     'shared.rich-text': TextBlock,
-    // 'shared.media': Image
+    'shared.media': Image
 };
