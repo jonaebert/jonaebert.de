0b97cb7a3327975f0ad3374c8724ec3b41a5b79f
Jona Ebert <kontakt@jonaebert.de>
2025-07-16 21:35:26 +0200

    Füge API Token in Umgebung hinzu

--- src/lib/store.js
+++ src/lib/store.js
@@ -25,6 +25,7 @@ export const je_api_base_url = import.meta.env.VITE_JE_API_BASE_URL;
 
 // CMS
 export const je_cms_api_base_url = import.meta.env.VITE_JE_CMS_API_BASE_URL;
+export const je_cms_api_token = import.meta.env.VITE_JE_CMS_API_TOKEN;
 
 // Budibase
 export const bb_base_url = import.meta.env.VITE_BB_BASE_URL;
