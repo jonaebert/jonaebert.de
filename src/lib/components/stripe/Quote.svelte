e62762a174e8182a345fe9548452eb080f0b6892
Jona Ebert <kontakt@jonaebert.de>
2025-07-21 12:21:14 +0200

    Füge Margin-bottom hinzu für trennung der einzelnen Elemente

--- src/lib/components/stripe/Quote.svelte
+++ src/lib/components/stripe/Quote.svelte
@@ -4,31 +4,33 @@
 	export let position: string;
 </script>
 
-<figure class="max-w-screen-md text-start bg-grey-200 p-6 rounded-lg md:rounded-xl">
-	<svg
-		class="size-7.5 mb-3 text-gray-400"
-		aria-hidden="true"
-		xmlns="http://www.w3.org/2000/svg"
-		fill="currentColor"
-		viewBox="0 0 18 14"
-	>
-		<path
-			d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"
-		/>
-	</svg>
-	<blockquote>
-		<p class="text-2xl italic font-medium font-poppins text-secondary-900">
-			{body}
-		</p>
-	</blockquote>
-	{#if name}
-		<figcaption class="flex items-center justofy-center mt-6 space-x-3 rtl:space-x-reverse font-montserrat">
-			<div class="flex items-center divide-x-1 rtl:divide-x-reverse divide-grey-600">
-				<cite class="pe-3 font-medium text-gray-900">{name}</cite>
-				{#if position}
-					<cite class="ps-3 text-sm text-gray-500">{position}</cite>
-				{/if}
-			</div>
-		</figcaption>
-	{/if}
-</figure>
+<div class="prose max-w-none mb-6">
+	<figure class="max-w-screen-md text-start bg-grey-200 p-6 rounded-lg md:rounded-xl">
+		<svg
+			class="size-7.5 mb-3 text-gray-400"
+			aria-hidden="true"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="currentColor"
+			viewBox="0 0 18 14"
+		>
+			<path
+				d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"
+			/>
+		</svg>
+		<blockquote>
+			<p class="text-2xl italic font-medium font-poppins text-secondary-900">
+				{body}
+			</p>
+		</blockquote>
+		{#if name}
+			<figcaption class="flex items-center justofy-center mt-6 space-x-3 rtl:space-x-reverse font-montserrat">
+				<div class="flex items-center divide-x-1 rtl:divide-x-reverse divide-grey-600">
+					<cite class="pe-3 font-medium text-gray-900">{name}</cite>
+					{#if position}
+						<cite class="ps-3 text-sm text-gray-500">{position}</cite>
+					{/if}
+				</div>
+			</figcaption>
+		{/if}
+	</figure>
+</div>
