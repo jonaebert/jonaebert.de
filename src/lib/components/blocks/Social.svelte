3db8dcb3844c9c3754031439b56d55328e4b8325
Jona Ebert <kontakt@jonaebert.de>
2026-01-04 14:50:09 +0100

    Add new social media icons and improve color handling in RoundedIcon component

--- src/lib/components/blocks/Social.svelte
+++ src/lib/components/blocks/Social.svelte
@@ -1,38 +1,39 @@
 <script lang="ts">
     import RoundedIcon from "$lib/components/button/RoundedIcon.svelte"
-    import { faInstagram, faThreads, faYoutube, faTiktok, faLinkedinIn, faGithub, faMastodon } from "@fortawesome/free-brands-svg-icons"; // For new Icons: https://fontawesome.com/search
+    import { faInstagram, faThreads, faYoutube, faTiktok, faLinkedinIn, faGithub, faMastodon, faBluesky, faFacebook } from "@fortawesome/free-brands-svg-icons"; // For new Icons: https://fontawesome.com/search
     // Farben
     let colors = {
         default: {
             icon: "white",
-            background: "secondary-900",
+            background: "bg-secondary-800",
             border: "gray-300",
         },
         focus: {
             icon: "black",
-            background: "white",
+            background: "bg-white",
             border: "himmel-600",
         },
         hover: {
             icon: "secondary-900",
-            background: "sun-600",
+            background: "bg-sun-600",
             border: "transparent",
         },
         disabled: {
             icon: "white",
-            background: "gray-300",
+            background: "bg-gray-300",
             border: "transparent",
         },
     };
-    let disabled = false;
 </script>
 
 <div class="gap-x-3.5 gap-y-3.5 grid grid-cols-4 md:grid-cols-4">
-    <div><RoundedIcon href="https://www.instagram.com/jonaebertbs" icon={faInstagram} target="_blank" {colors} {disabled} /></div>
-    <div><RoundedIcon href="https://www.threads.net/@jonaebertbs" icon={faThreads} target="_blank" {colors} {disabled} /></div>
-    <div><RoundedIcon href="https://mastodon.social/@jonaebertbs" icon={faMastodon} target="_blank" {colors} {disabled} /></div>
-    <div><RoundedIcon href="https://www.youtube.com/@jonaebertbs" icon={faYoutube} target="_blank" {colors} {disabled} /></div>
-    <div><RoundedIcon href="https://www.tiktok.com/@jona_ebert" icon={faTiktok} target="_blank" {colors} {disabled} /></div>
-    <div><RoundedIcon href="https://www.linkedin.com/in/jonaebert" icon={faLinkedinIn} target="_blank" {colors} {disabled} /></div>
-    <div><RoundedIcon href="https://github.com/jonaebert" icon={faGithub} target="_blank" {colors} {disabled} /></div>
+    <div><RoundedIcon href="https://www.instagram.com/jonaebertbs" icon={faInstagram} target="_blank" {colors} /></div>
+    <div><RoundedIcon href="https://www.threads.net/@jonaebertbs" icon={faThreads} target="_blank" {colors} /></div>
+    <div><RoundedIcon href="https://www.facebook.com/profile.php?id=61585717786862" icon={faFacebook} target="_blank" {colors} /></div>
+    <div><RoundedIcon href="https://mastodon.social/@jonaebertbs" icon={faMastodon} target="_blank" {colors} /></div>
+    <div><RoundedIcon href="https://bsky.app/profile/jonaebertbs.bsky.social" icon={faBluesky} target="_blank" {colors} /></div>
+    <div><RoundedIcon href="https://www.youtube.com/@jonaebertbs" icon={faYoutube} target="_blank" {colors} /></div>
+    <div><RoundedIcon href="https://www.tiktok.com/@jonaebertbs" icon={faTiktok} target="_blank" {colors} /></div>
+    <div><RoundedIcon href="https://www.linkedin.com/in/jonaebert" icon={faLinkedinIn} target="_blank" {colors} /></div>
+    <div><RoundedIcon href="https://github.com/jonaebert" icon={faGithub} target="_blank" {colors} /></div>
 </div>
--- src/lib/components/button/RoundedIcon.svelte
+++ src/lib/components/button/RoundedIcon.svelte
@@ -26,38 +26,32 @@
             border:         "",
         },
     };
-    export let disabled = false;
 
     // Funktion zum Anwenden der Farbklassen
-    function applyColorClasses(colors) {
+    function applyColorClasses(colors: { default: any; focus: any; hover: any; disabled: any; }) {
         return {
             default: {
                 icon: colors.default.icon ? "text-" + colors.default.icon : "text-black",
-                background: colors.default.background ? "bg-" + colors.default.background : "bg-white",
+                background: colors.default.background ? colors.default.background : "bg-white",
                 border: colors.default.border ? "border-" + colors.default.border : "border-gray-300",
             },
             focus: {
                 icon: colors.focus.icon ? "focus:text-" + colors.focus.icon : "focus:text-black",
-                background: colors.focus.background ? "focus:bg-" + colors.focus.background : "focus:bg-white",
+                background: colors.focus.background ? "focus:" + colors.focus.background : "focus:bg-white",
                 border: colors.focus.border ? "focus:border-" + colors.focus.border : "focus:border-himmel-600",
             },
             hover: {
                 icon: colors.hover.icon ? "hover:text-" + colors.hover.icon : "hover:text-black",
-                background: colors.hover.background ? "hover:bg-" + colors.hover.background : "hover:bg-sun-600",
+                background: colors.hover.background ? "hover:" + colors.hover.background : "hover:bg-sun-600",
                 border: colors.hover.border ? "hover:border-" + colors.hover.border : "hover:border-transparent",
-            },
-            disabled: {
-                icon: colors.disabled.icon ? "disabled:text-" + colors.disabled.icon : "disabled:text-white",
-                background: colors.disabled.background ? "disabled:bg-" + colors.disabled.background : "disabled:bg-gray-300",
-                border: colors.disabled.border ? "disabled:border-" + colors.disabled.border : "disabled:border-transparent",
-            },
+            }
         };
     }
     // Farben anwenden
     const appliedColors = applyColorClasses(colors);
 </script>
 
-<button class="inline-flex items-center justify-center p-3 border focus:border-2 disabled:cursor-not-allowed rounded-full transition-transform duration-400 hover:scale-110 cursor-pointer {appliedColors.default.icon} {appliedColors.focus.icon} {appliedColors.hover.icon} {appliedColors.disabled.icon} {appliedColors.default.background} {appliedColors.focus.background} {appliedColors.hover.background} {appliedColors.disabled.background} {appliedColors.default.border} {appliedColors.focus.border} {appliedColors.hover.border} {appliedColors.disabled.border}" title={icon.iconName} on:click={() => window.open(href, target)} {disabled}>
+<button class="inline-flex items-center justify-center p-3 border focus:border-2 disabled:cursor-not-allowed rounded-full transition-transform duration-400 hover:scale-110 cursor-pointer {appliedColors.default.icon} {appliedColors.focus.icon} {appliedColors.hover.icon} {appliedColors.default.background} {appliedColors.focus.background} {appliedColors.hover.background} {appliedColors.default.border} {appliedColors.focus.border} {appliedColors.hover.border}" title={icon.iconName} on:click={() => window.open(href, target)}>
     <div class="w-5 h-5 flex justify-center">
         <Fa {icon} size="lg" />
     </div>
