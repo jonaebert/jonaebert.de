40467d499bacc93e402ded9364160ab31c5bebab
Jona Ebert <kontakt@jonaebert.de>
2026-01-04 16:09:22 +0100

    Replace button element with anchor tag for improved link behavior in RoundedIcon component

--- src/lib/components/button/RoundedIcon.svelte
+++ src/lib/components/button/RoundedIcon.svelte
@@ -64,8 +64,8 @@
 	].join(' ');
 </script>
 
-<button class={buttonClasses} title={icon.iconName} on:click={() => window.open(href, target)}>
+<a {href} {target} rel={target === '_blank' ? 'noopener noreferrer' : undefined} class={buttonClasses} title={icon.iconName} role="button">
 	<div class="w-5 h-5 flex justify-center">
 		<Fa {icon} size="lg" />
 	</div>
-</button>
+</a>
