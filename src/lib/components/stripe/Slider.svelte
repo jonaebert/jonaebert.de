b790efa3dee97a6f671f7b4eec171b4e6b41faa0
Jona Ebert <kontakt@jonaebert.de>
2026-02-06 22:26:24 +0100

    fix: update base URL references in Image and Slider components

--- src/lib/components/stripe/Slider.svelte
+++ src/lib/components/stripe/Slider.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { je_cms_api_base_url } from '$lib/store.js'
+	import { je_cms_base_url } from '$lib/store.js'
 	import Image from '$lib/components/Image.svelte';
 	import InfoMessage from '$lib/components/blocks/InfoMessage.svelte';
 
@@ -7,9 +7,9 @@
 
 	function getMediaURL(media:type) {
 		if (media?.ext === '.svg') {
-			return je_cms_api_base_url + media?.url;
+			return je_cms_base_url + media?.url;
 		} else if (media?.formats?.large?.url) {
-			return je_cms_api_base_url + media?.formats?.large?.url;
+			return je_cms_base_url + media?.formats?.large?.url;
 		}
 	}
 
