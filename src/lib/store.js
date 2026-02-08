7afeda6d6dff1a1f5b070eb5b3eacaf7927b7a8c
Jona Ebert <kontakt@jonaebert.de>
2026-02-08 22:00:24 +0100

    fix: update image URLs to use dynamic base URL for better maintainability

--- src/lib/store.js
+++ src/lib/store.js
@@ -33,12 +33,12 @@ export const n8n_contact_url = import.meta.env.VITE_JE_N8N_CONTACT_URL;
 
 // Pictures
 // Logo
-export const logo = 'https://cms.jonaebert.de/uploads/Logo_310x100_c17dfefe52.svg';
-export const logo_clear = 'https://cms.jonaebert.de/uploads/Logo_310x100_Clear_2769e089a9.svg';
-export const logo_small = 'https://cms.jonaebert.de/uploads/Logo_500x500_67fca42684.svg';
-export const logo_small_clear = 'https://cms.jonaebert.de/uploads/Logo_500x500_Clear_dce31db3d5.svg';
+export const logo = `${je_cms_base_url}/uploads/Logo_310x100_c17dfefe52.svg`;
+export const logo_clear = `${je_cms_base_url}/uploads/Logo_310x100_Clear_2769e089a9.svg`;
+export const logo_small = `${je_cms_base_url}/uploads/Logo_500x500_67fca42684.svg`;
+export const logo_small_clear = `${je_cms_base_url}/uploads/Logo_500x500_Clear_dce31db3d5.svg`;
 // Alte Waage
-export const img_alte_waage = 'https://cms.jonaebert.de/uploads/medium_Braunschweig_Alte_Waage_ced5cdd56e.png';
+export const img_alte_waage = `${je_cms_base_url}/uploads/medium_Braunschweig_Alte_Waage_ced5cdd56e.png`;
 
 // Block image copy and context menu
 export function contextMenuAction(node) {
