0a4393c4d19cdd5c4d69258981143b6d571f7552
Jona Ebert <kontakt@jonaebert.de>
2026-02-08 22:06:08 +0100

    fix: update load function to be asynchronous for improved data fetching

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,11 @@
-import type { PageLoad } from "./$types";
-import { api } from "$lib/api";
+import type { PageLoad } from './$types';
+import { api } from '$lib/api';
 
-export const load: PageLoad = ({ fetch }) => {
-  const client = api(fetch);
-  return {
-    posts: client.get("/blog/posts/", { query: { limit: 8 } }),
-    events: client.get("/calendar/events/", { query: { limit: 8 } })
-  };
+export const load: PageLoad = async ({ fetch, params }) => {
+	const client = api(fetch);
+
+	return {
+		posts: client.get('/blog/posts/', { query: { limit: 8 } }),
+		events: client.get('/calendar/events/', { query: { limit: 8 } })
+	};
 };
