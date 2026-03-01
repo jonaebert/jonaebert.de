5be6d436eb796c5bcdf570d65007bbfbf3de41c9
Jona Ebert <kontakt@jonaebert.de>
2026-03-01 07:01:09 +0100

    Changed listenplatz rat to 14

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -509,7 +509,7 @@
 				</h3>
 
 				<div class="text-sm sm:text-base text-zinc-600 dark:text-zinc-400">
-					Listenplatz <span class="font-semibold">8</span> für den Rat<br />
+					Listenplatz <span class="font-semibold">14</span> für den Rat<br />
 					Listenplatz <span class="font-semibold">1</span> für den{' '}
 					<span class="relative inline-flex" bind:this={rootEl}>
 						<button
