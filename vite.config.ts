a32f4fc0b9c67dc88f9df7ed389713238fa6b5fe
Jona Ebert <kontakt@jonaebert.de>
2026-03-18 18:58:45 +0100

    Moved to @tailwindcss/vite and removed postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 const REQUIRED = [
 	'VITE_JE_API_BASE_URL',
@@ -22,6 +23,6 @@ export default defineConfig(({ mode }) => {
 	}
 
 	return {
-		plugins: [sveltekit()]
+		plugins: [sveltekit(), tailwindcss()]
 	};
 });
