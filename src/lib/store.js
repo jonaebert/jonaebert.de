05c94100a3a7b5044a5a05dad3a41a2413de7765
Jona Ebert <kontakt@jonaebert.de>
2026-05-06 19:25:28 +0200

    fix: standardize formatting and indentation in store.js

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,22 +1,22 @@
 // store.js
-import { page } from "$app/stores"; // Values: https://developer.mozilla.org/en-US/docs/Web/API/URL
+import { page } from '$app/stores'; // Values: https://developer.mozilla.org/en-US/docs/Web/API/URL
 
 // Personal
-export const name       = 'Jona Ebert';
-export const pronouns   = 'they/them';
-export const job        = 'Systemadministrator:in';
-export const slogan     = 'Grün. Gemeinsam. Sozial.';
+export const name = 'Jona Ebert';
+export const pronouns = 'they/them';
+export const job = 'Systemadministrator:in';
+export const slogan = 'Grün. Gemeinsam. Sozial.';
 export const address = {
-    street: 'Postfach 5835',
-    zipcode: '38049',
-    city: 'Braunschweig',
-    country: 'Deutschland'
+	street: 'Postfach 5835',
+	zipcode: '38049',
+	city: 'Braunschweig',
+	country: 'Deutschland'
 };
 export const contact = {
-    fax: '+49 5307 9402832',
-    mail: 'kontakt@jonaebert.de',
-    web: '/contact'
-}
+	fax: '+49 5307 9402832',
+	mail: 'kontakt@jonaebert.de',
+	web: '/contact'
+};
 
 // URI
 export const uri = page;
@@ -41,18 +41,18 @@ export const logo_clear = `${je_cms_base_url}/uploads/Logo_310x100_Clear_2769e08
 export const logo_small = `${je_cms_base_url}/uploads/Logo_500x500_67fca42684.svg`;
 export const logo_small_clear = `${je_cms_base_url}/uploads/Logo_500x500_Clear_dce31db3d5.svg`;
 // Alte Waage
-export const img_alte_waage = `${je_cms_base_url}/uploads/medium_Braunschweig_Alte_Waage_ced5cdd56e.png`;
+export const img_alte_waage = `${je_cms_base_url}/uploads/Braunschweig_Alte_Waage_da98b0abaa.avif`;
 
 // Block image copy and context menu
 export function contextMenuAction(node) {
-    node.oncontextmenu = event => {
-        event.preventDefault();
-    };
-    node.draggable = false;
+	node.oncontextmenu = (event) => {
+		event.preventDefault();
+	};
+	node.draggable = false;
 
-    return {
-        destroy() {
-            node.oncontextmenu = null;
-        }
-    };
+	return {
+		destroy() {
+			node.oncontextmenu = null;
+		}
+	};
 }
